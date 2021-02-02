414119f7dd7bed029bd7c7af08738826fddbe74f
Shadow <root@syned.cn>
2021-02-02 10:47:11 +0800

    修改视图转发

--- src/main/java/cn/syned/crm/commons/controller/ViewController.java
+++ src/main/java/cn/syned/crm/commons/controller/ViewController.java
@@ -2,8 +2,8 @@ package cn.syned.crm.commons.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpServletRequest;
 import java.io.File;
 
 @Controller
@@ -11,44 +11,57 @@ public class ViewController {
 
     //======================================Workbench=======================================
 
-    @GetMapping(path = {"/workbench/{view}", "/workbench/", "/workbench/index.html"})
-    public String toWorkBenchView(
-            @PathVariable(value = "view", required = false) String view) {
-        if (view == null) {
-            return "/WEB-INF/workbench/index.jsp";
-        }
-        return "/WEB-INF/workbench" + File.separator + view;
-    }
-
-    @GetMapping(path = "/workbench/{module}/{view}")
-    public String toWorkBenchSubView(
-            @PathVariable("module") String module,
-            @PathVariable("view") String view) {
-        return "/WEB-INF/workbench" + File.separator + module + File.separator + view;
-    }
+//    @GetMapping(path = {"/workbench/{view}", "/workbench/", "/workbench/index.html"})
+//    public String toWorkBenchView(
+//            @PathVariable(value = "view", required = false) String view) {
+//        if (view == null) {
+//            return "/WEB-INF/workbench/index.jsp";
+//        }
+//        return "/WEB-INF/workbench" + File.separator + view;
+//    }
+//
+//    @GetMapping(path = "/workbench/{module}/{view}")
+//    public String toWorkBenchSubView(
+//            @PathVariable("module") String module,
+//            @PathVariable("view") String view) {
+//        return "/WEB-INF/workbench" + File.separator + module + File.separator + view;
+//    }
 
     //======================================Settings=======================================
 
-    @GetMapping(path = {"/settings/{view}", "/settings/"})
-    public String toSettingsView(
-            @PathVariable(value = "view", required = false) String view) {
-        if (view == null) {
-            return "/WEB-INF/settings/index.html";
-        }
-        return "/WEB-INF/settings" + File.separator + view;
+//    @GetMapping(path = {"/settings/{view}", "/settings/"})
+//    public String toSettingsView(
+//            @PathVariable(value = "view", required = false) String view) {
+//        if (view == null) {
+//            return "/WEB-INF/settings/index.html";
+//        }
+//        return "/WEB-INF/settings" + File.separator + view;
+//    }
+//
+//    @GetMapping(path = "/settings/{module}/{view}")
+//    public String toSettingsModuleView(
+//            @PathVariable("module") String module,
+//            @PathVariable(value = "view") String view) {
+//        return "/WEB-INF/settings" + File.separator + module + File.separator + view;
+//    }
+//
+//    @GetMapping(path = "/settings/{module}/{submodule}/{view}")
+//    public String toSettingsSubModuleView(
+//            @PathVariable("module") String module,
+//            @PathVariable("submodule") String submodule,
+//            @PathVariable(value = "view") String view) {
+//        return "/WEB-INF/settings" + File.separator + module + File.separator + submodule + File.separator + view;
+//    }
+
+    @GetMapping(path = "/settings/**")
+    public String toSettings(HttpServletRequest request) {
+        String path = request.getServletPath();
+        return "/WEB-INF" + File.separator + path;
     }
 
-    @GetMapping(path = "/settings/{module}/{view}")
-    public String toSettingsSubView(
-            @PathVariable("module") String module,
-            @PathVariable(value = "view") String view) {
-        return "/WEB-INF/settings" + File.separator + module + File.separator + view;
-    }
-
-    @GetMapping(path = "/settings/dictionary/{module}/{view}")
-    public String toSettingsSubsView(
-            @PathVariable("module") String module,
-            @PathVariable(value = "view") String view) {
-        return "/WEB-INF/settings/dictionary" + File.separator + module + File.separator + view;
+    @GetMapping(path = "/workbench/**")
+    public String toWorkBench(HttpServletRequest request) {
+        String path = request.getServletPath();
+        return "/WEB-INF" + File.separator + path;
     }
 }
